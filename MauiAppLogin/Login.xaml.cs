c7b14db094c67bd9f44057131fb5a511539fd8a2
HoneyMoon22698 <2404872@escolas.anchieta.br>
2025-03-03 18:18:58 -0300

    Finalizacao do projeto Login

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace MauiAppLogin;
 
 public partial class Login : ContentPage
@@ -5,5 +7,47 @@ public partial class Login : ContentPage
 	public Login()
 	{
 		InitializeComponent();
+	}
+
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			List<DadosUsuario> Lista_usuarios = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "Jose",
+					Senha = "123"
+				},
+				new DadosUsuario()
+				{
+					Usuario = "Maria",
+					Senha = "321"
+				}
+			};
+
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+				Senha = txt_senha.Text
+			};
+
+			//Link
+			if(Lista_usuarios.Any(i => dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha))
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+			} else
+			{
+				throw new Exception("Usuário ou Senha incorretos.");
+			}
+
+
+		} catch(Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+		}
 	}
 }
